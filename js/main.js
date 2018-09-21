8c3e02d0cc5f4fc7a458611f3fe3e428108cdf57
johnkegz <johnkal24@gmail.com>
2018-09-21 01:46:50 -0700

    Chore(Login): Integrate login page with api[#160681303]

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-document.getElementById('addpost').addEventListener('submit', addPost);
+//document.getElementById('addpost').addEventListener('submit', addPost);
+document.getElementById('login-form').addEventListener('submit', Login);
 function copyDiv(){    
     var firstDivContent = document.getElementById('navv');
     var secondDivContent = document.getElementById('menu-display');
@@ -10,7 +11,7 @@ function copyDiv2(){
     var secondDivContent = document.getElementById('menu-display');
     secondDivContent.innerHTML = "";
 }
-document.getElementById('get-posts').addEventListener('click', addQuestion);
+//document.getElementById('get-posts').addEventListener('click', addQuestion);
 //document.getElementById('get-post').addEventListener('click', getOneQuestion);
 function addQuestion(){
 	fetch('http://127.0.0.1:5000/api/v1/questions')
@@ -104,6 +105,26 @@ function addPost(e){
 			
         },
         body: JSON.stringify({user_name: user_name, email: email, pass_word: pass})
+    })
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    
+}
+//Login
+function Login(e){
+	alert(0);
+    e.preventDefault();
+    let email = document.getElementById('email').value;
+	let pass = document.getElementById('password').value;
+
+    fetch('http://127.0.0.1:5000/api/v1/auth/login', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-type':'application/json',
+			'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify({email: email, pass_word: pass})
     })
     .then((res) => res.json())
     .then((data) => console.log(data))
