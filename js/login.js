1df373142cabc7c9ea47830b60bebf4826f97a4f
johnkegz <johnkal24@gmail.com>
2018-09-25 18:29:21 -0700

    Host app to heroku

--- js/login.js
+++ js/login.js
@@ -1,4 +1,14 @@
-
+function copyDiv(){    
+    var firstDivContent = document.getElementById('navv');
+    var secondDivContent = document.getElementById('menu-display');
+    secondDivContent.innerHTML = firstDivContent.innerHTML;
+}
+  
+function copyDiv2(){
+    var firstDivContent = document.getElementById('navv');
+    var secondDivContent = document.getElementById('menu-display');
+    secondDivContent.innerHTML = "";
+}
 document.getElementById('login-form').addEventListener('submit', Login);
 
 //Login
