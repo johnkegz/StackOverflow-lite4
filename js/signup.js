addb58eced426ef98b9362a14720a8ff47509a6a
johnkegz <johnkal24@gmail.com>
2018-09-26 11:05:52 -0700

    Host to heroku

--- js/signup.js
+++ js/signup.js
@@ -7,7 +7,10 @@ function addPost(e){
     let user_name = document.getElementById('user_name').value;
     let email = document.getElementById('email').value;
 	let pass = document.getElementById('password').value;
-
+	let pass2 = document.getElementById('confirm_password').value;
+    
+	if(pass == pass2){
+		
     fetch('https://stackoverflow-lite2.herokuapp.com/api/v1/auth/signup', {
         method: 'POST',
         headers: {
@@ -19,6 +22,20 @@ function addPost(e){
         body: JSON.stringify({user_name: user_name, email: email, pass_word: pass})
     })
     .then((res) => res.json())
-    .then((data) => console.log(data))
+    .then((data) => {
+		let message = data.message;
+		localStorage.setItem("accessToken", data.access_token)
+		console.log(message);
+		if(message == 'you have successfully created an account'){
+			alert(message);
+			window.location = 'home.html';
+		}
+		else {
+			alert(message);
+		}
+		})
     
 }
+else{
+	alert("Password doesnot match");
+}}
